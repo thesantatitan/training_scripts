6622a31c19956f7c6214c090d5ef0e3d8a55994e
Dev Rajput <devrajput060901@gmail.com>
2024-12-19 01:23:54 +0530

    basic setup done

--- setup.sh
+++ setup.sh
@@ -6,3 +6,8 @@ apt install -y build-essentials
 #install uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
+git clone https://github.com/thesantatitan/training_scripts.git
+cd training_scripts
+uv venv
+source .venv/bin/activate
+uv pip install -r requirements.txt
