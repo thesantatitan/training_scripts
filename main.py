36f34ea6155d954ef7a8de722c9ff8cb25ecc994
Dev Rajput <devrajput060901@gmail.com>
2024-12-19 01:41:08 +0530

    remove setup.sh

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ import modal
 import torch
 
 app = modal.App("example-get-started")  # creating an App
-image = modal.Image.debian_slim().add_local_file('./setup.sh', '/setup.sh', copy=True).run_commands('chmod +x /setup.sh && /setup.sh')  # creating an Image with a local file
+image = (
+    modal.Image.debian_slim()
+    .apt_install(["git", "build-essential"])
+    .run_commands('git clone https://github.com/thesantatitan/training_scripts.git')
+    .run_commands('cd training_scripts && uv add -r requirements.txt && uv add -r flux-control/requirements.txt')
+)
+
 
 @app.function(gpu="H100", image=image)  # defining a Modal Function with a GPU
 def check_gpus():
